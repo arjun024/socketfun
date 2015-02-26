a098a831b62f578448468d73c6f40408b8732768
Arjun Sreedharan <arjun024@gmail.com>
2015-02-26 21:51:56 +0530

    chat: add lot of comments
    also update README, add Makefile
    Signed-off-by: Arjun Sreedharan <arjun024@gmail.com>

--- chat/chatclient.c
+++ chat/chatclient.c
@@ -1,7 +1,8 @@
 /*
-* Author:  Arjun Sreedharan
-* License: GPL version 2 or higher
+* Copyright (C) 2015  Arjun Sreedharan
+* License: GPL version 2 or higher http://www.gnu.org/licenses/gpl.html
 */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -17,6 +18,12 @@
 static unsigned short port = 55555;
 static char username[USERNAME_MAX_SIZE];
 
+/*
+* Conditional variables (@console_cv) permit us to wait until
+* another thread completes an arbitrary activity.
+* A mutex (@console_cv_lock) is required to protect the condition
+* variable itself from race condition.
+*/
 pthread_cond_t console_cv;
 pthread_mutex_t console_cv_lock;
 
@@ -30,34 +37,63 @@ void console(int sockfd)
 {
 	char buffer[BUFF_SIZE];
 	char *recipient, *msg, *tmp;
+
 	memset(buffer, 0, sizeof buffer);
 	printf("%s\n%s\n", "Welcome to chat client console. Please enter commands",
 		"syntax: [command] [optional recipient] [optional msg]");
+
+	/*
+	* Issue the prompt and wait for command,
+	* process the command and
+	* repeat forever
+	*/
 	while(1) {
+		/* console prompt */
 		printf("[%s]$ ", username);
 		fgets(buffer, sizeof buffer, stdin);
+		/* fgets also reads the \n from stdin, strip it */
 		buffer[strlen(buffer) - 1] = '\0';
 
 		if(strcmp(buffer, "") == 0)
 			continue;
 
 		if(strncmp(buffer, "exit", 4) == 0) {
+			/* tell server to clean up structures for the client */
 			write(sockfd, "exit", 6);
+			/* clean up self and exit */
 			pthread_mutex_destroy(&console_cv_lock);
 			pthread_cond_destroy(&console_cv);
 			_exit(EXIT_SUCCESS);
 		}
 
+		/*
+		* `ls` is sent to server to get list of connected users.
+		* It is written to server's socket, then using conditional wait,
+		* we `wait` until the reply arrives in the receiver thread, where
+		* `signal` is done immediately when the reply is read
+		*/
 		if(strncmp(buffer, "ls", 2) == 0) {
+			/*
+			* The mutex the protects the conditional has to
+			* be locked before a conditional wait.
+			*/
 			pthread_mutex_lock(&console_cv_lock);
 			write(sockfd, "ls", 2);
 			/* not protected from spurious wakeups */
+			/*
+			* This operation unlocks the given mutex and waits until a 
+			* pthread_cond_signal() happens on the same conditonal variable.
+			* Then the given mutex is again unlocked
+			*/
 			pthread_cond_wait(&console_cv, &console_cv_lock);
+			/* release the mutex */
 			pthread_mutex_unlock(&console_cv_lock);
 			continue;
 		}
 
+		/* `send <recipient> <msg>` sends <msg> to the given <username> */
 		if(strncmp(buffer, "send ", 5) == 0) {
+			/* the following is to validate the syntax */
 			tmp = strchr(buffer, ' ');
 			if(tmp == NULL) {
 				error();
@@ -72,6 +108,7 @@ void console(int sockfd)
 			}
 			msg = tmp + 1;
 
+			/* issue the `send` command to server */
 			write(sockfd, buffer, 5 + strlen(recipient) + 1 + strlen(msg) + 1);
 			continue;
 		}
@@ -80,6 +117,10 @@ void console(int sockfd)
 	}
 }
 
+/*
+* write username to server
+* in the syntax: register username <username>
+*/
 void register_username(int sockfd)
 {
 	char *regstring = malloc(USERNAME_MAX_SIZE + 18);
@@ -88,6 +129,11 @@ void register_username(int sockfd)
 	free(regstring);
 }
 
+/*
+* the stupid receiver thread
+* It continuously waits for messages from the server,
+* and prints it whatever it is.
+*/
 void *receiver(void *sfd)
 {
 	char buffer[BUFF_SIZE] = {0};
@@ -100,41 +146,68 @@ void *receiver(void *sfd)
 			continue;
 		pthread_mutex_lock(&console_cv_lock);
 		printf("%s\n", buffer);
+		/* let the other thread stop waiting, if it is */
 		pthread_cond_signal(&console_cv);
 		pthread_mutex_unlock(&console_cv_lock);
-		/* flush because previous printf doesn't end in \n */
-		fflush(stdout);
 	}
-	return NULL;
 }
 
-int main()
+int main(void)
 {
 	int sockfd;
+	/*
+	* this is the container for socket's address that contains
+	* address family, ip address, port
+	*/
 	struct sockaddr serv_addr;
 	char filler[16] = {0};
+	/* just to dump the handle for the spawned thread - no use */
 	pthread_t receiver_thread;
 
 	pthread_cond_init(&console_cv, NULL);
 	pthread_mutex_init(&console_cv_lock, NULL);
 
+	/*
+	* creates a socket of family Internet sockets (AF_INET) and
+	* of type stream. 0 indicates to system to choose appropriate
+	* protocol (eg: TCP)
+	*/
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
+	/*
+	* Address represented by struct sockaddr:
+	* first 2 bytes: Address Family,
+	* next 2 bytes: port,
+	* next 2 bytes: ipaddr,
+	* next 8 bytes: zeroes
+	*/
+	/*
+	* htons() and htonl() change endianness to
+	* network order which is the standard for network
+	* communication.
+	*/
 	filler[0] = (unsigned short)AF_INET;
 	filler[2] = (unsigned short)htons(port);
 	filler[4] = (unsigned long)htonl(INADDR_ANY);
+
+	/* copy data in the filler buffer to the socket address */
 	memcpy(&serv_addr, filler, sizeof serv_addr);
 
+	/* binds a socket to an address */
 	bind(sockfd, &serv_addr, sizeof serv_addr);
 
+	/* makes connection per the socket address */
 	connect(sockfd, &serv_addr, sizeof serv_addr);
 
 	printf("%s\n", "Enter a username (max 20 characters, no spaces):");
 	fgets(username, sizeof username, stdin);
+	/* fgets also reads the \n from stdin, strip it */
 	username[strlen(username) - 1] = '\0';
 
 	register_username(sockfd);
+	/* spawn a new thread that continuously listens for any msgs from server */
 	pthread_create(&receiver_thread, NULL, receiver, (void*)&sockfd);
+	/* get our console in action, let the user enter commands */
 	console(sockfd);
 
 	return 0;
--- chat/chatserver.c
+++ chat/chatserver.c
@@ -1,7 +1,8 @@
 /*
-* Author:  Arjun Sreedharan
-* License: GPL version 2 or higher
+* Copyright (C) 2015  Arjun Sreedharan
+* License: GPL version 2 or higher http://www.gnu.org/licenses/gpl.html
 */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -16,16 +17,26 @@
 
 static unsigned short port = 55555;
 
+/* client_node abstracts a connected client */
 struct client_node {
 	int sockfd;
 	char username[USERNAME_MAX_SIZE];
 	struct client_node *next;
 };
 
+/*
+* a list of `client_node`s which serves as our
+* connected client list
+*/
 struct client_node *client_list = NULL;
 
+/*
+* any operation on the client list is to be performed
+* only after getting a lock on this mutex
+*/
 pthread_mutex_t client_list_lock;
 
+/* add to the linked list - no rocket science */
 struct client_node *add_client(int cfd)
 {
 	struct client_node *p = client_list;
@@ -33,23 +44,33 @@ struct client_node *add_client(int cfd)
 	c->sockfd = cfd;
 	c->username[0] = '\0';
 	c->next = NULL;
+	/* always get a lock before you mess with list */
+	pthread_mutex_lock(&client_list_lock);
 	while(p && p->next)
 		p = p->next;
 	if(p == NULL)
 		client_list = c;
 	else
 		p->next = c;
+	/* release the lock when we are done with */
+	pthread_mutex_unlock(&client_list_lock);
 	return c;
 }
 
+/*
+* return NULL if no node is present with username @recipient,
+* else return pointer to the node
+*/
 struct client_node *search_client_list(char *recipient)
 {
 	struct client_node *p = client_list;
 	if(recipient == NULL || *recipient == '\0')
 		return NULL;
+	/* I am a law abiding citizen, wait until I get a lock */
 	pthread_mutex_lock(&client_list_lock);
 	while(p != NULL) {
 		if(strcmp(p->username, recipient) == 0) {
+			/* never forget to release the lock, don't you like freedom */
 			pthread_mutex_unlock(&client_list_lock);
 			return p;
 		}
@@ -59,14 +80,17 @@ struct client_node *search_client_list(char *recipient)
 	return NULL;
 }
 
+/* remove the client fromt he list of clients */
 void remove_client(struct client_node *c)
 {
 	struct client_node *p, *prev;
 	p = client_list;
 	prev = NULL;
+	/* get a lock and only then touch the list */
 	pthread_mutex_lock(&client_list_lock);
 	if(p == c) {
 		client_list = p->next;
+		pthread_mutex_unlock(&client_list_lock);
 		return;
 	}
 	while(p != c && p != NULL) {
@@ -82,6 +106,10 @@ char *get_username(struct client_node *cnode)
 {
 	char *str = malloc(38);
 	read(cnode->sockfd, str, 38);
+	/*
+	* gives pointer to the character after the second space,
+	* assuming str to be "register username <username>"
+	*/
 	return strrchr(str, ' ') + 1;
 }
 
@@ -90,33 +118,57 @@ void *handle_client(void* c)
 	char buffer[BUFF_SIZE] = {0};
 	struct client_node *cnode, *targetnode, *tmpnode;
 	char *recipient, *msg, *tmp, *formatted_msg;
+	/* cast the void* pointer back to its original type */
 	cnode = (struct client_node *)c;
+
+	/*
+	* expecting client program's register_username() to send
+	* a msg of syntax "register username <username>"
+	* This parsed by get_username() to retrieve username,
+	* and then copied to the client's node.
+	*/
 	strcpy(cnode->username, get_username(cnode));
-	printf("user: %s, socket: %d, thread:%lu\n", 
+	/* logging in the server */
+	printf("user: %s, socket: %d, thread:%lu\n",
 		cnode->username, cnode->sockfd, pthread_self());
 
+	/*
+	* read each `instruction` from the client,
+	* take approprite action, and then come back to beginning
+	* of the loop to wait for further instructions
+	*/
 	while(1) {
 		memset(buffer, 0, sizeof buffer);
+		/* read() call blocks till receipt of a msg */
 		read(cnode->sockfd, buffer, sizeof buffer);
 
 		if(strncmp(buffer, "exit", 4) == 0) {
+			/* clean up when a client quits */
 			remove_client(cnode);
 			close(cnode->sockfd);
 			free(cnode);
 		}
 
+		/*
+		* `ls` lists all clients' usernames that are
+		* currently connected
+		*/
 		if(strncmp(buffer, "ls", 2) == 0) {
 			memset(buffer, 0, sizeof buffer);
 			tmpnode = client_list;
 			while(tmpnode) {
+				/* concatenate each username to the buffer */
 				strcat(buffer, tmpnode->username);
 				strcat(buffer, "\n");
 				tmpnode = tmpnode->next;
 			}
+			/* write the buffer to cient's socket */
 			write(cnode->sockfd, buffer, strlen(buffer));
 		}
 
+		/* `send <recipient> <msg>` sends <msg> to the given <username> */
 		if(strncmp(buffer, "send ", 5) == 0) {
+			/* parse buffer to separate recipient and msg */
 			tmp = strchr(buffer, ' ');
 			if(tmp == NULL)
 				continue;
@@ -127,20 +179,33 @@ void *handle_client(void* c)
 				continue;
 			*tmp = '\0';
 			msg = tmp + 1;
+
+			/* search for the recipient in the cient list */
 			targetnode = search_client_list(recipient);
+
+			/* on invalid recipient, do nothing */
 			if(targetnode == NULL)
 				continue;
+
 			formatted_msg = malloc(BUFF_SIZE);
-			if(BUFF_SIZE < strlen(cnode->username) + strlen(msg))
+			/* sprint is notorious for buffer overflow */
+			if(BUFF_SIZE < strlen(cnode->username) + strlen(msg) + 2)
 				continue;
+			/* create a string of syntax `<sender>: <msg>` to send to recipient */
 			sprintf(formatted_msg, "%s: %s", cnode->username, msg);
+			/* logging in the server */
 			printf("%s sent msg to %s\n", cnode->username, targetnode->username);
+			/* Hey target client, You've got message ;) */
 			write(targetnode->sockfd, formatted_msg, strlen(formatted_msg) + 1);
 			free(formatted_msg);
 		}
 	}
 
-	/* exit */
+	/*
+	* clean up
+	* Let me tell you a secret, this never happens
+	* Oh, wait !!
+	*/
 	pthread_mutex_destroy(&client_list_lock);
 	close(cnode->sockfd);
 	return NULL;
@@ -149,37 +214,83 @@ void *handle_client(void* c)
 int main(void)
 {
 	int sockfd, client_sockfd;
+	/*
+	* this is the container for socket's address that contains
+	* address family, ip address, port
+	*/
 	struct sockaddr serv_addr, client_addr;
 	char filler[16] = {0};
 	unsigned int supplied_len;
 	unsigned int *ip_suppliedlen_op_storedlen;
+	/* just to dump the handle for the spawned thread - no use */
 	pthread_t thread;
 
+	/*
+	* initiate a mutex to protect the client list from
+	* being accessed by multiple threads at the same time
+	*/
 	pthread_mutex_init(&client_list_lock, NULL);
 
+	/*
+	* creates a socket of family Internet sockets (AF_INET) and
+	* of type stream. 0 indicates to system to choose appropriate
+	* protocol (eg: TCP)
+	*/
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-	memset(&serv_addr, 0, sizeof serv_addr);
+	/*
+	* Address represented by struct sockaddr:
+	* first 2 bytes: Address Family,
+	* next 2 bytes: port,
+	* next 2 bytes: ipaddr,
+	* next 8 bytes: zeroes
+	*/
+	/*
+	* htons() and htonl() change endianness to
+	* network order which is the standard for network
+	* communication.
+	*/
 	filler[0] = (unsigned short)AF_INET;
 	filler[2] = (unsigned short)htons(port);
 	filler[4] = (unsigned long)htonl(INADDR_ANY);
+
+	/* copy data in the filler buffer to the socket address */
 	memcpy(&serv_addr, filler, sizeof(serv_addr));
 
+	/* binds a socket to an address */
 	bind(sockfd, &serv_addr, sizeof serv_addr);
 
+	/*
+	* allows the process to listen on the socket for given
+	* max number of connections
+	*/
 	listen(sockfd, 5);
 
+	/*
+	* This ptr on input specifies the length of the supplied sockaddr,
+	* and on output specifies the length of the stored address
+	*/
 	supplied_len = sizeof(client_addr);
 	ip_suppliedlen_op_storedlen = &supplied_len;
 
+	/*
+	* Now ready to accept clients -
+	* For each client, a new client_node is created and added to the client list.
+	* To handle the client, a new thread is spawned handled by handle_client().
+	* Then, we let the main thread come back to the beginning of the loop to wait for
+	* further clients while the newly spawned thread deals with the accepted client.
+	*/
 	while(1) {
 		struct client_node *cnode;
+		/*
+		* causes the process to block until a client connects to the server,
+		* returns a new file descriptor to communicate with the connected client
+		*/
 		client_sockfd = accept(sockfd, &client_addr, ip_suppliedlen_op_storedlen);
 
-		pthread_mutex_lock(&client_list_lock);
 		cnode = add_client(client_sockfd);
-		pthread_mutex_unlock(&client_list_lock);
 
+		/* pass a pointer to the correspond client node to the new thread's handler */ 
 		pthread_create(&thread, NULL, handle_client, (void*)cnode);
 	}
 	return 0;
